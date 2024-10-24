7f6fb419c00f4a0276aafa6d1dbeffb3b3efd46c
Luca <152995292+sky-luca@users.noreply.github.com>
2024-10-24 20:19:27 +0200

    Add files via upload

---
+++ halloween/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'  -- Or 'cerulean', 'adamant', etc.
+game 'gta5'
+
+author 'Luca'
+description 'Halloween Map'
+version '1.0.0'
+
+-- This is required for map resources
+this_is_a_map 'yes'
+
+
+--Made by luca
+--https://www.luca-info.com
+--Support: https://www.luca-info.com
